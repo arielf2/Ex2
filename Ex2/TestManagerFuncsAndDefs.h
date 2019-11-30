d6684193523da793afa205b211d1447fd97d955f
arielf2 <yamiyami260@gmail.com>
2019-11-30 13:30:42 +0200

    fix test manager works but still need to add error handling, free resources etc

--- Ex2/TestManagerFuncsAndDefs.h
+++ Ex2/TestManagerFuncsAndDefs.h
@@ -9,5 +9,12 @@
 #define ID_LENGTH 10
 #define COMMAND_LINE_LEN 30
 
+
+typedef struct _STUDENT_INFO {
+	char id[ID_LENGTH];
+	int *grade;
+} STUDENT_INFO;
+
 BOOL CreateProcessSimple(LPTSTR CommandLine, PROCESS_INFORMATION* ProcessInfoPtr);
 void CreateCommandLine(char* output_command, char* id);
+int WriteFinalGrades(STUDENT_INFO *stud_array, int num_of_students);
--- Ex2/main.c
+++ Ex2/main.c
@@ -2,24 +2,20 @@
 
 int main(int argc, char *argv[])
 {
-	char id[ID_LENGTH + 1] = ""; /* need extra char for \n */
 	PROCESS_INFORMATION procinfo_array[MAX_NUM_OF_STUDENTS];
 	DWORD				waitcode;
 	BOOL				retVal;
 	HANDLE p_process_handles[MAX_NUM_OF_STUDENTS];
-	DWORD  exitcode_array[MAX_NUM_OF_STUDENTS];
+	STUDENT_INFO student_array[MAX_NUM_OF_STUDENTS]; //char id[10], int* grade;
+	FILE *fp = NULL;
+	char id[ID_LENGTH + 1] = ""; /* need extra char for \n */
 	int proc_count = 0;
 	int file_error = 0;
-	char ids_array[MAX_NUM_OF_STUDENTS][ID_LENGTH];
-	FILE *fp = NULL;
-	char students_id_path[100] = "";
 	
-	strcat_s(&students_id_path, 100, argv[1]);
 	if (_chdir(argv[1])) {
 		printf("Error opening Grades Directory\n");
 		return 1;
 	}
-
 	//GetCurrentDirectory(200, &cur_dir);
 	file_error = fopen_s(&fp, "student_ids.txt", "r");
 	if (file_error != 0) {
@@ -31,15 +27,13 @@ int main(int argc, char *argv[])
 		printf("Error changing directories\n");
 	}
 	while (fgets(&id, sizeof(id), fp)) {
-		
 		id[ID_LENGTH - 1] = '\0'; /* strip the line of \n*/
-		strcpy_s(ids_array[proc_count], ID_LENGTH, &id);
-		
+		strcpy_s(student_array[proc_count].id, ID_LENGTH, id);
+
 		char command_line[COMMAND_LINE_LEN] = "TestGrade.exe ";
 		if (strcat_s(command_line, 30, id)) {
 			printf("Error concatenating id to program name\n");
 		}
-
 		retVal = CreateProcessSimple(command_line, &(procinfo_array[proc_count]));
 		if (retVal == 0) {
 			printf("Process Creation Failed with error: %d\n", GetLastError());
@@ -48,33 +42,45 @@ int main(int argc, char *argv[])
 	}
 	fclose(fp);
 
+	/* Populate process handles array for WaitForMultipleObjects */
 	for (int i = 0; i < proc_count; i++) {
 		p_process_handles[i] = procinfo_array[i].hProcess;
 	}
 	
 	waitcode = WaitForMultipleObjects(proc_count, p_process_handles, TRUE, INFINITE);
-	// arielcheck waitfor multiple objects - the wait is stuck
-	printf("waitcode = %d with error code %d\n", waitcode, GetLastError());
-	// returns waitcode -1: wait failed. maybe something wrong with p process handles
 	if (waitcode == WAIT_TIMEOUT) /* Processeseseses is still alive */ {
 		/* kill all processes?*/
 	}
 	printf("waitcode = %d with error code %d\n", waitcode, GetLastError());
 	/* check all wait codes */
 
+	/* Get ExitCodes (calculated average) from each process (student) */
 	for (int j = 0; j < proc_count; j++) {
-		GetExitCodeProcess(procinfo_array[j].hProcess, &(exitcode_array[j]));/*averag*/
+		GetExitCodeProcess(procinfo_array[j].hProcess, &student_array[j].grade);
+		//check exit code process
 	}
-	/* write to final file
-	concat ids_array[i] and exitcode_array[i] */
 
-	printf("proc_count is: %d\n", proc_count);
+	WriteFinalGrades(student_array, proc_count);
 	return 0;
-
 }
 
 
 
+int WriteFinalGrades(STUDENT_INFO *stud_array, int num_of_students) {
+	//printf("address:%s\ndata:%s\n", &ids_array[0], ids_array[0]);
+	int file_error, j;
+	FILE *fp = NULL;
+	file_error = fopen_s(&fp, "final_grades.txt", "w");
+	if (file_error != 0) {
+		printf("Error opening final_grades.txt for writing\n");
+		return 1;
+	}
+	for (j = 0; j < num_of_students; j++) {
+		fprintf(fp, "%s %d\n", stud_array[j].id, stud_array[j].grade);
+	}
+	fclose(fp);
+}
+MyFunc();
 BOOL CreateProcessSimple(LPTSTR CommandLine, PROCESS_INFORMATION* ProcessInfoPtr)
 {
 	STARTUPINFO	startinfo = { sizeof(STARTUPINFO), NULL, 0 }; /* <ISP> here we */
