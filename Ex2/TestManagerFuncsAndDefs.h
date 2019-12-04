34c59202e551ce0da0f8e05061cf4bcaad7b8da9
arielf2 <yamiyami260@gmail.com>
2019-12-04 18:35:35 +0200

    update test manager it will now send the following to testgrade: TestGrade.exe {grades_dir}//grades_{id}

--- Ex2/TestManagerFuncsAndDefs.h
+++ Ex2/TestManagerFuncsAndDefs.h
@@ -3,11 +3,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
-#include <direct.h>
+
 
 #define MAX_NUM_OF_STUDENTS 10
 #define ID_LENGTH 10
-#define COMMAND_LINE_LEN 30
+#define COMMAND_LINE_LEN 50
+#define MAX_RELATIVE_PATH_LEN 100 //input(5) + space(1) + id(9)
+#define MAX_OUTPUT_FILE_LEN 100
+#define STUDENT_FILE "//student_ids.txt"
+#define OUTPUT_GRADE_FILE "//final_grades.txt"
+#define TEST_GRADE_PROG "TestGrade.exe "
+#define GRADES_DIR "//grades_"
 
 
 typedef struct _STUDENT_INFO {
@@ -16,5 +22,4 @@ typedef struct _STUDENT_INFO {
 } STUDENT_INFO;
 
 BOOL CreateProcessSimple(LPTSTR CommandLine, PROCESS_INFORMATION* ProcessInfoPtr);
-void CreateCommandLine(char* output_command, char* id);
-int WriteFinalGrades(STUDENT_INFO *stud_array, int num_of_students);
+int WriteFinalGrades(STUDENT_INFO *StudArray, int NumOfStudents, char* OutputFile);
--- Ex2/main.c
+++ Ex2/main.c
@@ -9,78 +9,113 @@ int main(int argc, char *argv[])
 	STUDENT_INFO student_array[MAX_NUM_OF_STUDENTS]; //char id[10], int* grade;
 	FILE *fp = NULL;
 	char id[ID_LENGTH + 1] = ""; /* need extra char for \n */
-	int proc_count = 0;
-	int file_error = 0;
-	
-	if (_chdir(argv[1])) {
-		printf("Error opening Grades Directory\n");
-		return 1;
-	}
+	char StudentFileRelativePath[MAX_RELATIVE_PATH_LEN] = ""; // change before submitting because running from CMD is directly in DEBUG folder
+	char OutputFileRelativePath[MAX_OUTPUT_FILE_LEN] = "";
+	int proc_count = 0, file_error = 0, error_flag = 0, k = 0;
+	int num_of_processes_created = 0;
+
+	//char cur_dir[100] = "";
 	//GetCurrentDirectory(200, &cur_dir);
-	file_error = fopen_s(&fp, "student_ids.txt", "r");
+	//printf("father current directory is: %s\n", cur_dir);
+
+	strcat_s(StudentFileRelativePath, sizeof(StudentFileRelativePath), argv[1]); // input
+	strcat_s(StudentFileRelativePath, sizeof(StudentFileRelativePath), STUDENT_FILE); //input//student_ids.txt
+
+	file_error = fopen_s(&fp, StudentFileRelativePath, "r");
 	if (file_error != 0) {
-		printf("Error opening student_ids.txt\n");
+		printf("Error opening %s\n", StudentFileRelativePath);
 		return 1;
 	}
-	/* loop for creating 10 processes, one for each student */
-	if (_chdir("..//..//Debug//")) {
-		printf("Error changing directories\n");
-	}
+	/*
+	#define TEST_GRADE_PROG "..//Debug//TestGrade.exe "
+	#define GRADES_DIR "//grades_"
+	*/
 	while (fgets(&id, sizeof(id), fp)) {
 		id[ID_LENGTH - 1] = '\0'; /* strip the line of \n*/
 		strcpy_s(student_array[proc_count].id, ID_LENGTH, id);
 
-		char command_line[COMMAND_LINE_LEN] = "TestGrade.exe ";
-		if (strcat_s(command_line, 30, id)) {
-			printf("Error concatenating id to program name\n");
-		}
+		char command_line[COMMAND_LINE_LEN] = TEST_GRADE_PROG;
+		
+		//command line should be input//grades_123456789
+		strcat_s(command_line, sizeof(command_line), argv[1]);
+		strcat_s(command_line, sizeof(command_line), GRADES_DIR);
+		strcat_s(command_line, sizeof(command_line), id);
+
+		//command line should be TestGrade.exe {argv[1]}//grades_{id}
+
+
 		retVal = CreateProcessSimple(command_line, &(procinfo_array[proc_count]));
 		if (retVal == 0) {
-			printf("Process Creation Failed with error: %d\n", GetLastError());
+			printf("Process Creation failed with error: %d.\nTerminating processes 0 to %d and exiting", GetLastError(), proc_count - 1);
+			//num_of_processes_created = proc_count;
+			//for (k = 0; k < proc_count; k++)
+			//	TerminateProcess(p_process_handles[k], 100); //change to brutal termination code
+			error_flag = 1;
+			break;
 		}
+		/* Populate process handles array for WaitForMultipleObjects */
+		p_process_handles[proc_count] = procinfo_array[proc_count].hProcess;
 		proc_count++;
 	}
+
+	// proc_count tells us how many processes were created successfully. so we can iterate from 0 to proc_count - 1 to terminate 
+	// all the created processes s
 	fclose(fp);
+	if (!error_flag) { // if we're here all the processes were created successfully and their number is proc_count
 
-	/* Populate process handles array for WaitForMultipleObjects */
-	for (int i = 0; i < proc_count; i++) {
-		p_process_handles[i] = procinfo_array[i].hProcess;
+		waitcode = WaitForMultipleObjects(proc_count, p_process_handles, TRUE, INFINITE);
+		if (waitcode == WAIT_FAILED | waitcode == WAIT_ABANDONED_0 | waitcode == WAIT_TIMEOUT) {
+			error_flag = 1;
+			printf("WaitForMultipleObjects delivered bad waitcode %d with error %d\n", waitcode, GetLastError());
+			//for (k = 0; k < proc_count; k++)
+			//	TerminateProcess(p_process_handles[k], 100); //change to brutal termination code
+		}
 	}
-	
-	waitcode = WaitForMultipleObjects(proc_count, p_process_handles, TRUE, INFINITE);
-	if (waitcode == WAIT_TIMEOUT) /* Processeseseses is still alive */ {
-		/* kill all processes?*/
-	}
-	printf("waitcode = %d with error code %d\n", waitcode, GetLastError());
-	/* check all wait codes */
-
 	/* Get ExitCodes (calculated average) from each process (student) */
-	for (int j = 0; j < proc_count; j++) {
-		GetExitCodeProcess(procinfo_array[j].hProcess, &student_array[j].grade);
-		//check exit code process
+	/* need to fix since testgrade exitcode doesn't need to be grades */
+	if (!error_flag) {
+		for (int j = 0; j < proc_count; j++) {
+			GetExitCodeProcess(procinfo_array[j].hProcess, &student_array[j].grade);
+			if (student_array[j].grade < 0 | student_array[j].grade > 100) {
+				/* bad exitcode	*/
+				printf("Captain, we were unable to calculate %s", student_array[j].id);
+				error_flag = 1;
+			}
+		}
 	}
-
-	WriteFinalGrades(student_array, proc_count);
+	if (!error_flag) {
+		strcat_s(OutputFileRelativePath, sizeof(OutputFileRelativePath), argv[1]);
+		/* need to open final_{id} and read grade from there */
+		WriteFinalGrades(student_array, proc_count, OutputFileRelativePath);
+	}
+	/* close handles*/
+	//for (k = 0; k < proc_count; k++) {
+	//	TerminateProcess(p_process_handles[k], 100); //change to termination code
+	//	CloseHandle(p)
+	//}
 	return 0;
 }
 
 
 
-int WriteFinalGrades(STUDENT_INFO *stud_array, int num_of_students) {
+int WriteFinalGrades(STUDENT_INFO *StudArray, int NumOfStudents, char* OutputFile) {
 	//printf("address:%s\ndata:%s\n", &ids_array[0], ids_array[0]);
 	int file_error, j;
 	FILE *fp = NULL;
-	file_error = fopen_s(&fp, "final_grades.txt", "w");
+	strcat_s(OutputFile, MAX_OUTPUT_FILE_LEN , OUTPUT_GRADE_FILE);
+	file_error = fopen_s(&fp, OutputFile, "w");
 	if (file_error != 0) {
 		printf("Error opening final_grades.txt for writing\n");
 		return 1;
 	}
-	for (j = 0; j < num_of_students; j++) {
-		fprintf(fp, "%s %d\n", stud_array[j].id, stud_array[j].grade);
+	for (j = 0; j < NumOfStudents; j++) {
+		printf("%s %d\n", StudArray[j].id, StudArray[j].grade); //print on console
+		fprintf(fp, "%s %d\n", StudArray[j].id, StudArray[j].grade);
+		
 	}
 	fclose(fp);
 }
-MyFunc();
+
 BOOL CreateProcessSimple(LPTSTR CommandLine, PROCESS_INFORMATION* ProcessInfoPtr)
 {
 	STARTUPINFO	startinfo = { sizeof(STARTUPINFO), NULL, 0 }; /* <ISP> here we */
@@ -102,9 +137,3 @@ BOOL CreateProcessSimple(LPTSTR CommandLine, PROCESS_INFORMATION* ProcessInfoPtr
 	);
 }
 
-
-//CreateCommandLine(&command_line, "TestGrade.exe"); /* TestGrade.txt 123456789 */
-void CreateCommandLine(char* output_command, char* id)
-{
-	strcat_s(output_command, ID_LENGTH, id);
-}
